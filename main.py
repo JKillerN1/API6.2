1498b45b5e89578df6d96cc0670bfd490d5856fb
Arina <arina2004popova@yandex.ru>
2022-12-04 15:38:10 +0300

    extra files were deleted when an error occurred

--- main.py
+++ main.py
@@ -16,25 +16,30 @@ def dowload_picture(filename, url):
 
 
 if __name__ == '__main__':
+    load_dotenv()
     comic_number = random.randint (0,1000)
     filename = f'comic_img{comic_number}.png'
     url = f"https://xkcd.com/{comic_number}/info.0.json"
-    dowload_picture(filename, url)
 
-    load_dotenv()
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
+    try:
+        dowload_picture(filename, url)
 
-    app_id = os.getenv('APP_ID')
-    vk_session = vk_api.VkApi(login, password, app_id)
-    vk_session.auth()
-    upload = VkUpload(vk_session)
-    photos = filename
-    photo_list = upload.photo_wall(photos)
-    attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
-    vk_session.method("wall.post", {
-        'owner_id': os.getenv('OWNER_ID'),
-        'attachment': attachment,
-    })
+        login = os.getenv('LOGIN')
+        password = os.getenv('PASSWORD')
+        app_id = os.getenv('APP_ID')
+        vk_session = vk_api.VkApi(login, password, app_id)
+        vk_session.auth()
+        upload = VkUpload(vk_session)
+        photos = filename
+        photo_list = upload.photo_wall(photos)
+        attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
+        vk_session.method("wall.post", {
+            'owner_id': os.getenv('OWNER_ID'),
+            'attachment': attachment,
+        })
 
-   
+        os.remove(photos)
+
+    except ValueError:
+        os.remove(filename)
+
